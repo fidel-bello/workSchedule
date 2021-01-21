b5a9b84e8acc27adaefa1a703da9059173596163
fidel bello <59423@bears.smcc.edu>
2021-01-21 18:54:38 -0500

    function for the document and onlcikc for the savebtn that gets values from description and saves to local storage

--- java.js
+++ java.js
@@ -3,3 +3,14 @@ var currentDay = moment().format("dddd, MMMM Do YYYY");
 // selected currentDay p id to display the current date
 $("#currentDay").html(currentDay);
 
+//fucntionality for entire document
+$(document).ready(function(){
+    //onlick on save btn
+    $("saveBtn").on("click", function(){
+        //gets values from description
+        var input = $(this).sibling("description").val();
+        var time = $(this).parent().attr("id");
+        //save user input in local storage and time
+        localStorage.setItem(time, input);
+    })
+});
