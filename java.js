9c2b4238ccd764ed2bc67023169d3edf90192af2
fidel bello <59423@bears.smcc.edu>
2021-01-21 23:26:29 -0500

    added placeholder tags

--- java.js
+++ java.js
@@ -2,15 +2,18 @@
 var currentDay = moment().format("dddd, MMMM Do YYYY");
 // selected currentDay p id to display the current date
 $("#currentDay").html(currentDay);
+var now = moment().format("H A")
 
 //fucntionality for entire document
 $(document).ready(function(){
     //onlick on save btn
     $("saveBtn").on("click", function(){
         //gets values from description
+        var now = moment().format("H A");
         var input = $(this).sibling("description").val();
         var time = $(this).parent().attr("id");
         //save user input in local storage and time
         localStorage.setItem(time, input);
     })
 });
+ 
