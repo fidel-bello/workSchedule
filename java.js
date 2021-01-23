5efeacafcafccd19c70f9ebdbb9e670c100ff680
fidel bello <59423@bears.smcc.edu>
2021-01-22 22:21:04 -0500

    added get local storage method for each timeblock

--- java.js
+++ java.js
@@ -1,10 +1,21 @@
 //declared variable for current date
-var currentDay = moment().format("dddd, MMMM Do YYYY");
+var currentDay = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 // selected currentDay p id to display the current date
 $("#currentDay").html(currentDay);
 
 //fucntionality for entire document
 $(document).ready(function(){
+        //gets local storage from the val of the timeblock 8 am
+        $("#hour8 .description").val(localStorage.getItem("hour8"));
+        $("#hour9 .description").val(localStorage.getItem("hour9"));
+        $("#hour10 .description").val(localStorage.getItem("hour10"));
+        $("#hour11 .description").val(localStorage.getItem("hour11"));
+        $("#hour12 .description").val(localStorage.getItem("hour12"));
+        $("#hour13 .description").val(localStorage.getItem("hour13"));
+        $("#hour14 .description").val(localStorage.getItem("hour14"));
+        $("#hour15 .description").val(localStorage.getItem("hour15"));
+        $("#hour16 .description").val(localStorage.getItem("hour16"));
+        $("#hour17 .description").val(localStorage.getItem("hour17"));
     //onlick on save btn
     $(".saveBtn").on("click", function(){
         //gets values from description
@@ -42,8 +53,7 @@ $(document).ready(function(){
             }
         })
     }
-    //gets local storage from the val of the timeblock 8 am
-    $("#hour8 .description").val(localStorage.getItem("hour8"));
+
     addColor()
 });
  
