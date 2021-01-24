7dc7fcd09ef71c168161d5d727cdb4604abb0c4a
fidel bello <59423@bears.smcc.edu>
2021-01-23 19:14:28 -0500

    fixed bug where hour 18 was not color coded

--- assets/java.js
+++ assets/java.js
@@ -16,6 +16,7 @@ $(document).ready(function(){
  $("#hour15 .description").val(localStorage.getItem("hour15"));
  $("#hour16 .description").val(localStorage.getItem("hour16"));
  $("#hour17 .description").val(localStorage.getItem("hour17"));
+ $("#hour18 .description").val(localStorage.getItem("hour18"));
     //onlick on save btn
     $(".saveBtn").on("click", function(){
         //gets values from description
@@ -32,7 +33,7 @@ $(document).ready(function(){
 
         //looping time blcosk
         $(".time-block").each(function (){
-            var time = parseInt($(this).attr("id").split("hour")[1]);
+            var time = parseInt($(this).attr("id").split("-")[1]);
 
             //checking if time of the block is less than current time then add the css class past
             if (time < now) {
